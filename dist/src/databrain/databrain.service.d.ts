8135bdcd1def4527f07a669d63f832d03fb00a0a
YinXiaoyu-1998 <consyinxy@gmail.com>
2026-02-11 15:28:49 +0800

    chore: clean repo and update gitignore

--- dist/src/databrain/databrain.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { HttpService } from '@nestjs/axios';
-export declare class DatabrainService {
-    private readonly httpService;
-    private readonly baseUrl;
-    constructor(httpService: HttpService);
-    loadFile(filepath: string, mimeType: string): Promise<void>;
-    aiChat(message: string): Promise<any>;
-}
--- dist/src/prisma/prisma.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-export declare class PrismaService implements OnModuleDestroy {
-    private readonly pool;
-    private readonly client;
-    constructor();
-    get prisma(): PrismaClient<import("@prisma/client").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/client").DefaultArgs>;
-    onModuleDestroy(): Promise<void>;
-}
